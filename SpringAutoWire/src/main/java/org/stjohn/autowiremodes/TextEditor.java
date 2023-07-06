c5e2d9fca8b4c14d1c563d8d828c226dfcb656f8
novinakoli <kolinovina@gmail.com>
2023-07-06 12:00:52 +0530

    first commit

---
+++ SpringAutoWire/src/main/java/org/stjohn/autowiremodes/TextEditor.java
@@ -0,0 +1,26 @@
+package org.stjohn.autowiremodes;
+
+public class TextEditor {
+	// DI in the form of objects
+	private SpellChecker checker;
+
+	// DI using constructor
+	public TextEditor(SpellChecker checker) {
+		super();
+		this.checker = checker;
+		System.out.println("DI using constructor");
+	}
+
+	// setters and getters method
+	public SpellChecker getChecker() {
+		return checker;
+	}
+
+	public void setChecker(SpellChecker checker) {
+		this.checker = checker;
+	}
+
+	public void display() {
+		checker.checkSpelling();
+	}
+}
